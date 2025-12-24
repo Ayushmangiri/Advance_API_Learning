e15473e7005af024d6d888e7bd458253e02afe21
Ayushmangiri <ayushmangiri@gmail.com>
2025-12-24 19:30:08 +0530

    first commit

---
+++ src/main/java/com/example/Advance_API_Learning/AdvanceApiLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.Advance_API_Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceApiLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceApiLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Advance_API_Learning/AdvanceApiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Advance_API_Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceApiLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
