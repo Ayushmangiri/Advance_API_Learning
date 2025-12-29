621ce8756290616ee7f0e67d9066ee0e0e38e757
Ayushmangiri <ayushmangiri@gmail.com>
2025-12-29 21:32:52 +0530

    Working on controller

--- src/main/java/com/example/Advance_API_Learning/controller/StudentsController.java
+++ src/main/java/com/example/Advance_API_Learning/controller/StudentsController.java
@@ -1,7 +1,15 @@
 package com.example.Advance_API_Learning.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class StudentsController {
 
+@RestController
+@RequestMapping ("/api/students")
+
+public class StudentsController {
+    @GetMapping ("/Login")
+    public String login(){
+        return  "Hey Ayushman you have sucessfully LogedIn";
+    }
 }
