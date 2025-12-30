d8b6daf8151c63a9187c582e64e3216264d0e36d
Ayushmangiri <ayushmangiri@gmail.com>
2025-12-30 22:26:01 +0530

    Working on controller

--- src/main/java/com/example/Advance_API_Learning/controller/StudentsController.java
+++ src/main/java/com/example/Advance_API_Learning/controller/StudentsController.java
@@ -1,15 +1,27 @@
 package com.example.Advance_API_Learning.controller;
 
+import com.example.Advance_API_Learning.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/api/students")
+@RequestMapping("/api/students")
 
 public class StudentsController {
-    @GetMapping ("/Login")
-    public String login(){
-        return  "Hey Ayushman you have sucessfully LogedIn";
+    @GetMapping("/login")
+    public String login() {
+        return "Hey Ayushman you have sucessfully LogedIn";
     }
+
+    @GetMapping("/student/details")
+    public Student getStudents(){
+    Student s1 = new Student();
+    s1.setName("AYUSHMAN");
+    s1.setCourse("MCA");
+        return s1;
+    }
+    @PostMapping ("/student")
+
 }
---
+++ src/main/java/com/example/Advance_API_Learning/entity/Student.java
@@ -0,0 +1,14 @@
+package com.example.Advance_API_Learning.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Student {
+    private String name;
+    private String course;
+
+}
